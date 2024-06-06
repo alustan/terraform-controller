3100136161dc7f362814b86b08fcd615428f69ce
ChichiCaleb <ChichiCaleb@chichiuchenna@gmail.com>
2024-06-06 13:01:15 -0700

    add oci registry

--- pkg/util/extract_env_vars.go
+++ pkg/util/extract_env_vars.go
@@ -1,14 +1,12 @@
 package util
 
 
-func ExtractEnvVars(variables, backend map[string]string) map[string]string {
+func ExtractEnvVars(variables map[string]string) map[string]string {
 	envVars := make(map[string]string)
 	for key, value := range variables {
 		envVars[key] = value
 	}
-	for key, value := range backend {
-		envVars[key] = value
-	}
+	
 	return envVars
 }
 

ecde3597622a44a61fc3600147ab701d6ff891c0
ChichiCaleb <ChichiCaleb@chichiuchenna@gmail.com>
2024-06-07 05:40:42 -0700

    update kaniko function

--- pkg/container/run_pod.go
+++ pkg/container/run_pod.go
@@ -8,6 +8,7 @@ import (
     metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
     apierrors "k8s.io/apimachinery/pkg/api/errors"
     "k8s.io/apimachinery/pkg/api/resource"
+    "k8s.io/apimachinery/pkg/types"
     "k8s.io/client-go/kubernetes"
 )
 
